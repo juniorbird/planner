2d9e87d88152621412d213eb22ed3ac1c3687bff
Wade Armstrong <wade@wadearmstrong.com>
2016-08-13 14:40:39 -0700

    Clean up code and add JSDoc comments

--- index.js
+++ index.js
@@ -8,10 +8,16 @@ const fs = require('fs');
 const testMD = fs.readFileSync('./test.md', 'UTF8');
 
 const cleanArgs = args => {
-  // First two args are:
-  //  1. Node
-  //  2. The running script
-  // Bleh, who needs those?
+  /**
+  * Given a set of command-line arguments
+  * Discard the first two args, which are
+  * 1. node
+  * 2. The running script
+  * Return an array of the remaining args
+  * @param args {array} always process.argv
+  * @returns {Array} An array of arguments, with - and -- removed
+  * @private
+  **/
 
   args = args.slice(2);
   return args.map(arg => {
@@ -21,6 +27,19 @@ const cleanArgs = args => {
 }
 
 const MDtoArr = openFile => {
+  /**
+  * Takes in a Markdown-formatted file
+  * Returns an array of questions and answers of the format
+  * [
+  *   { question: 'Text of question asked',
+  *     answer: 'Text of user answer' },
+  *   { question: 'Text of question asked 2',
+  *     answer: 'Text of user answer 2' },
+  * ]
+  * @param openFile {String} The result of fs.readFile for the Markdown-formatted file
+  * @returns {Array} An array of objects containing user questions and answers
+  * @private
+  **/
   let arr = /\n-*\n/g[Symbol.split](openFile);
   let question;
   let response;
@@ -39,6 +58,19 @@ const MDtoArr = openFile => {
 }
 
 const ArrToMD = questionArr => {
+  /**
+  * Takes in an array of questions and answers of the format
+  * [
+  *   { question: 'Text of question asked',
+  *     answer: 'Text of user answer' },
+  *   { question: 'Text of question asked 2',
+  *     answer: 'Text of user answer 2' },
+  * ]
+  * Returns a Markdown-formatted string that can be written into a file
+  * @param questionArr {Array} An array of objects containing user questions and answers
+  * @returns {String} A Markdown-formatted string that can be written to a file
+  * @private
+  **/
   let question;
   let response;
   return questionArr.reduce((prev, curr) => {
