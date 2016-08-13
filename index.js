fa466ff1b44ad1f7823292e96a92b88f27410e72
Wade Armstrong <wade@wadearmstrong.com>
2016-08-13 12:24:43 -0700

    can parse markdown into array of question objects

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 #!/usr/bin/env node
 
-
 'use strict';
 
-//process.argv.forEach(proc => console.log(proc));
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+const testMD = fs.readFileSync('./test.md', 'UTF8');
 
 const cleanArgs = args => {
   // First two args are:
@@ -18,6 +20,27 @@ const cleanArgs = args => {
   });
 }
 
-// console.log(__filename);
-// console.log(process.config)
-console.log('cleanargs', cleanArgs(process.argv));
+const MDtoArr = openFile => {
+  let re = /\n-*\n/g;
+  let arr = re[Symbol.split](openFile);
+  let question;
+  let response;
+  let red = arr.reduce((prev, curr, idx) => {
+    if (idx > 0) {
+      if ((idx % 2) === 1) {
+        question = curr;
+        response = '';
+      } else {
+        response = curr;
+      }
+      if (question && response) prev.push({ question: question, response: response});
+    }
+    return prev;
+  }, [])
+  return red;
+}
+
+
+// console.log('cleanargs', cleanArgs(process.argv));
+
+console.log(MDtoArr(testMD));
